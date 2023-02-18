5cfe45f4bed118f75cfa8d2d61a4585d64670fc2
vladislav5ik <33522722+vladislav5ik@users.noreply.github.com>
2023-02-19 00:52:21 +0300

    added a generation of random comics at the bottom of the page

---
+++ comic.js
@@ -0,0 +1,24 @@
+
+async function get_comic_id() {
+    let url = new URL("https://fwd.innopolis.app/api/hw2");
+    let params = {
+        email: "v.spigin@innopolis.university"
+    };
+    url.search = new URLSearchParams(params).toString();
+    return await fetch(url).then(response => response.json());
+}
+
+async function get_comic() {
+    let comic_id = await get_comic_id();
+    let url = new URL("https://getxkcd.vercel.app/api/comic");
+    let params = {
+        num: comic_id
+    };
+    url.search = new URLSearchParams(params).toString();
+    let comic = await fetch(url).then(response => response.json());
+    document.getElementById("comic_title").textContent = comic.title;
+    document.getElementById("comic_img").src = comic.img;
+    document.getElementById("comic_img").alt = comic.alt;
+    document.getElementById("comic_img").hidden = false;
+    document.getElementById("comic_date").textContent = new Date(comic.year, comic.month, comic.day).toLocaleDateString();
+}
